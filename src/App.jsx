0e352ebb6e21f36724614a3904e85f4d1aa5779b
KingAJ19 <anelesokwane@gmail.com>
2023-12-06 19:51:41 +0200

    api key

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState, useRef } from 'react'
 import './index.css'
 import { Form } from 'react-bootstrap'
 
+
+const API_URL = 'https://api.unsplash.com/search/photos';
+const API_KEY = '-CHxPUdU20SvjJocPcT6Q3XLRv5blB5LSLPTY1uMYrU'
+
 function App() {
 
   const searchInput = useRef(null);
 
   const handleSearch = (event) => {
       event.preventDefault();
+      console.log(searchInput.current.value)
+  }
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection
   }
 
   return (
@@ -17,6 +26,12 @@ function App() {
         <Form onSubmit={handleSearch}>
          <Form.Control type='Search' placeholder='Type Something To Search' className='search-input' ref={searchInput} />
         </Form>
+      </div>
+      <div className="filters">
+        <div onClick={() => handleSelection('nature')}>Nature</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
+        <div onClick={() => handleSelection('cats')}>Cats</div>
+        <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
     </div>
   )
