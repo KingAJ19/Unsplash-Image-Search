9ae4b07fa126ba6145f5308ba95d55484c33c8c9
KingAJ19 <anelesokwane@gmail.com>
2023-12-06 20:25:23 +0200

    finished it

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,106 @@
-import { useState, useRef, useEffect } from 'react'
-import './index.css'
 import axios from 'axios';
-import { Form } from 'react-bootstrap'
-
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const API_URL = 'https://api.unsplash.com/search/photos';
 const IMAGES_PER_PAGE = 20;
 
-
 function App() {
   const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const getImages = async () => {
-      
-  //   };
-
-  //   getImages();
-  // }, []);
-
-  const fetchImages = async () => {
+  const fetchImages = useCallback(async () => {
     try {
-      const result  = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`);
-      console.log('result', result.data);
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
     } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
       console.log(error);
+      setLoading(false);
     }
-  }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
 
   const handleSearch = (event) => {
-      event.preventDefault();
-      console.log(searchInput.current.value)
-  }
+    event.preventDefault();
+    resetSearch();
+  };
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
-    fetchImages();
-  }
+    resetSearch();
+  };
 
   return (
-    <div className="container">
-      <h1 className="title">Image Search</h1>
-      <div className="search-section">
+    <div className='container'>
+      <h1 className='title'>Image Search</h1>
+      {errorMsg && <p className='error-msg'>{errorMsg}</p>}
+      <div className='search-section'>
         <Form onSubmit={handleSearch}>
-         <Form.Control type='Search' placeholder='Type Something To Search' className='search-input' ref={searchInput} />
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
+          />
         </Form>
       </div>
-      <div className="filters">
+      <div className='filters'>
         <div onClick={() => handleSelection('nature')}>Nature</div>
         <div onClick={() => handleSelection('birds')}>Birds</div>
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
+      {loading ? (
+        <p className='loading'>Loading...</p>
+      ) : (
+        <>
+          <div className='images'>
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className='image'
+              />
+            ))}
+          </div>
+          <div className='buttons'>
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
