a9d6e5c74e5002572f9fb8f798ccbfcbc8d97b11
KingAJ19 <anelesokwane@gmail.com>
2023-12-06 19:36:46 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './index.css'
 import { Form } from 'react-bootstrap'
 
 function App() {
+
+  const searchInput = useRef(null);
+
+  const handleSearch = (event) => {
+      event.preventDefault();
+  }
+
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
       <div className="search-section">
-        <Form>
-        <Form.Control type='Search' placeholder='Type Something To Search' className='search-input' />
+        <Form onSubmit={handleSearch}>
+         <Form.Control type='Search' placeholder='Type Something To Search' className='search-input' ref={searchInput} />
         </Form>
       </div>
     </div>
