3a0e42ea77b26aba97aafd4e6ce301d314d6515a
KingAJ19 <anelesokwane@gmail.com>
2023-12-06 19:31:48 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './index.css'
+import { Form } from 'react-bootstrap'
 
 function App() {
   return (
-    <>
-     <div>
-       Hello
-     </div>
-    </>
+    <div className="container">
+      <h1 className="title">Image Search</h1>
+      <div className="search-section">
+        <Form>
+        <Form.Control type='Search' placeholder='Type Something To Search' className='search-input' />
+        </Form>
+      </div>
+    </div>
   )
 }
 
